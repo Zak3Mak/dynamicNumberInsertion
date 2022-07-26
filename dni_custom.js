d983f8ffc27c25da8fee5aa6e04a0c1e0713bceb
Zak3Mak <Zak3Mak@gmail.com>
2022-07-26 10:52:03 -0600

    Update dni_custom.js

--- dni_custom.js
+++ dni_custom.js
@@ -1,33 +1,23 @@
- const fpPromise = import('https://fpcdn.io/v3/i0B5iy6WSpMFPH0pSHLB')
-    .then(FingerprintJS => FingerprintJS.load({
-      endpoint: 'https://offers.goaptive.com'
-    }))
- 
-  fpPromise
-    .then(fp => fp.get())
-    .then(result => {
-      const visitorId = result.visitorId
-      console.log(visitorId)
-    });
-
+let visitorId = '';
+let dniData = '';
+let dniNumber = 8888888888;
 const pageUrl = window.location.href;
 //var dniNumber = findNumber(trackedNumber);
 const dniUrl = 'https://marketingservice-1986-dev.twil.io/DynamicNumberInsertion';
 const xhr = new XMLHttpRequest();
 
- 
 
-
-function findNumber(url, visitorId)
-{ 
-const dniData ="url="+pageUrl+"&visitorId="+visitorId; 
-xhr.open('POST', dniUrl, true);
-xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-xhr.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-    }
-}
-xhr.send(params);
-return xhr.responseText;
-}; 
+let fpPromise = import('https://fpcdn.io/v3/i0B5iy6WSpMFPH0pSHLB')
+.then(FingerprintJS => FingerprintJS.load())
+.then(fp => fp.get())
+.then(result => { 
+    visitorId = result.visitorId;
+    })
+.then (function() {dniData ="url="+pageUrl+"&visitorId="+visitorId;})
+.then (function (){xhr.open('POST', dniUrl, true);})
+.then (function (){xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');})
+.then (function (){xhr.onreadystatechange = function() {//Call a function when the state changes.
+    if(xhr.readyState == 4 && xhr.status == 200) {
+dniNumber=xhr.responseText; }
+}})
+.then (function(){xhr.send(dniData);});
