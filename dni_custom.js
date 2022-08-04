1821918f73303d515ba35d068ea7ae8e3fa810bb
Jeff Patten <42191597+JeffPatten@users.noreply.github.com>
2022-08-04 14:23:52 -0600

    Update dni_custom.js

--- dni_custom.js
+++ dni_custom.js
@@ -20,4 +20,32 @@ let fpPromise = import('https://fpcdn.io/v3/i0B5iy6WSpMFPH0pSHLB')
     if(xhr.readyState == 4 && xhr.status == 200) {
 dniNumber=xhr.responseText; }
 }})
-.then (function(){xhr.send(dniData);});
+.then (function(){xhr.send(dniData);})
+
+.then (function aptiveDisplayPhoneNumber(dniNumber) {
+    var cleaned = ('' + dniNumber).replace(/\D/g, '');
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      return '' + match[1] + '.' + match[2] + '.' + match[3];
+    }
+    return null;
+  };)
+.then (function aptiveHrefPhoneNumber(dniNumber) {
+    var cleaned = ('' + dniNumber).replace(/\D/g, '');
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      return '(' + match[1] + ')' + match[2] + '-' + match[3];
+    }
+    return null;
+  };)
+.then (function fillNumber(trackedNumber){
+    for (var i = 0; i < aTags.length; i++) {
+    if (aTags[i].href.search('tel:') == 0) {
+    aTags[i].href = 'tel:1'+aptiveHrefPhoneNumber(trackedNumber)+'';
+    aTags[i].dataset.ckeSavedHref = 'tel:1'+aptiveHrefPhoneNumber(trackedNumber)+'';
+    if (aTags[i].innerHTML.search('Call')== 0 )
+        {console.log("Mobile Page");}
+      else {aTags[i].innerHTML = aptiveDisplayPhoneNumber(trackedNumber);}
+    }
+  }
+};)
